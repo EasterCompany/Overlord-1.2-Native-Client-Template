572637846ff31d73f122bbab6a8a8afae734821f
Owen Easter <oweneaster@outlook.com>
2022-07-10 22:59:47 +0100

    10/JULY/22

--- App.tsx
+++ App.tsx
@@ -2,55 +2,69 @@
   This is Overlords basic native-client template; we recommend checking out the link below if this your first time
   using React-Native or Overlord's Native-Client. Further Documentation: https://reactnative.dev/docs/getting-started
 */
-import styles from './App.styles.ts'
+
+// Libs
+import React, { useState } from "react";
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, TouchableOpacity, Linking } from 'react-native';
+// Styles
+import theme from './App.theme.ts';
+// Components
+import Header from './components/header/header';
+import SlideModal from './components/modals/slideModal';
+import TutorialButtons from './components/example/tutorialButtons';
 
 
-const App = () => <View style={styles.background}>
-  <StatusBar style="auto" />
-  <View style={styles.view}>
-    <Text style={styles.normalHeader}>
-      Edit the 'App.tsx' file to start building your new app!
-    </Text>
+const App = () => {
+  const [menuModal, setMenuModal] = useState(false);
+  const [loginModal, setLoginModal] = useState(false);
 
-    <Text style={styles.text}>
-      Inside the terminal window running Overlord{'\n\n'}
-      Press 'W' to open the web-app.{'\n'}
-      Press 'A' to open the android-app.{'\n\n'}
-    </Text>
+  return <View style={theme.background}>
+    <StatusBar style="auto"/>
 
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => {
-        Linking.openURL('https://reactnative.dev/docs/intro-react');
-      }}>
-        <Text style={styles.buttonText}> Read Documentation </Text>
-      </TouchableOpacity>
+    <Header
+      menu={[menuModal, setMenuModal]}
+      login={[loginModal, setLoginModal]}
+    />
 
-      <TouchableOpacity style={styles.button} onPress={() => {
-        Linking.openURL('https://www.youtube.com/channel/UC6JMuccPLOKRL9cI95ZtJQQ');
-      }}>
-        <Text style={styles.buttonText}> Watch Tutorials </Text>
-      </TouchableOpacity>
+    <SlideModal
+      title="User Login"
+      state={[loginModal, setLoginModal]}
+    />
 
-      <TouchableOpacity style={styles.button} onPress={() => {
-        Linking.openURL('https://www.easter.company/');
-      }}>
-        <Text style={styles.buttonText}> Visit Easter.Company </Text>
-      </TouchableOpacity>
+    <View style={theme.view}>
+      <TextHeader/>
+      <TutorialButtons/>
+      <TextFooter/>
     </View>
 
-    <Text style={[styles.text, {marginTop: 50}]}>
-      The Overlord Framework
-    </Text>
-
-    <Text style={styles.hyperlink} onPress={() => {
-      Linking.openURL('https://github.com/EasterCompany/Overlord')
-    }}>
-      https://github.com/EasterCompany/Overlord
-    </Text>
   </View>
-</View>
+}
+
+
+const TextHeader = () => <>
+  <Text style={theme.normalHeader}>
+    Edit the 'App.tsx' file to start building your new app!
+  </Text>
+  <Text style={theme.text}>
+    Inside the terminal window running Overlord{'\n\n'}
+    Press 'W' to open in a web browser.{'\n'}
+    Press 'A' to open in a android emulator.{'\n'}
+    Press 'I' to open in a iOS emulator{'\n\n'}
+  </Text>
+</>
+
+
+const TextFooter = () => <>
+  <Text style={[theme.text, {marginTop: 50}]}>
+    The Overlord Framework
+  </Text>
+  <Text style={theme.hyperlink} onPress={() => {
+    Linking.openURL('https://github.com/EasterCompany/Overlord')
+  }}>
+    https://github.com/EasterCompany/Overlord
+  </Text>
+</>
 
 
 export default App;
